b1c59b49088ef1870edb9f942a02a5811d5d3349
Brendon Bown <brendon.bown@gmail.com>
2023-09-13 00:04:27 -0600

    Implement inverse cipher

--- src/inverse_cipher.rs
+++ src/inverse_cipher.rs
@@ -1,5 +1,4 @@
-use crate::constant::{KeyType, S_BOX};
-use crate::finite_field::FiniteField;
+use crate::{constant::{INV_S_BOX, KeyType}, finite_field::FiniteField, cipher::add_round_key};
 
 fn sub_bytes<S, T>(state: S) -> Vec<Vec<u8>>
 where
@@ -15,7 +14,7 @@ where
                 .map(|value| {
                     let row = (value >> 4) & 0b1111;
                     let column = value & 0b1111;
-                    S_BOX[row as usize][column as usize]
+                    INV_S_BOX[row as usize][column as usize]
                 })
                 .collect()
         })
@@ -38,7 +37,7 @@ where
             let mut new_row = vec![0; row.len()];
 
             for (j, value) in row.iter().enumerate() {
-                let new_index = (j + row.len() - i) % row.len();
+                let new_index = (j + i) % row.len();
                 new_row[new_index] = *value;
             }
 
@@ -65,13 +64,13 @@ where
 
     for c in 0..num_cols {
         result[0][c] =
-            (state[0][c] * 0x02.into()) + (state[1][c] * 0x03.into()) + state[2][c] + state[3][c];
+            (state[0][c] * 0x0e.into()) + (state[1][c] * 0x0b.into()) + (state[2][c] * 0x0d.into()) + (state[3][c] * 0x09.into());
         result[1][c] =
-            state[0][c] + (state[1][c] * 0x02.into()) + (state[2][c] * 0x03.into()) + state[3][c];
+            (state[0][c] * 0x09.into()) + (state[1][c] * 0x0e.into()) + (state[2][c] * 0x0b.into()) + (state[3][c] * 0x0d.into());
         result[2][c] =
-            state[0][c] + state[1][c] + (state[2][c] * 0x02.into()) + (state[3][c] * 0x03.into());
+            (state[0][c] * 0x0d.into()) + (state[1][c] * 0x09.into()) + (state[2][c] * 0x0e.into()) + (state[3][c] * 0x0b.into());
         result[3][c] =
-            (state[0][c] * 0x03.into()) + state[1][c] + state[2][c] + (state[3][c] * 0x02.into());
+            (state[0][c] * 0x0b.into()) + (state[1][c] * 0x0d.into()) + (state[2][c] * 0x09.into()) + (state[3][c] * 0x0e.into());
     }
 
     result
@@ -80,38 +79,7 @@ where
         .collect()
 }
 
-pub fn add_round_key<S, T>(state: S, expanded_key: &[u32], round_key: usize, n_b: usize) -> Vec<Vec<u8>>
-where
-    S: AsRef<[T]>,
-    T: AsRef<[u8]>,
-{
-    let state: Vec<Vec<u8>> = state
-        .as_ref()
-        .iter()
-        .map(AsRef::as_ref)
-        .map(|row| row.iter().copied().collect())
-        .collect();
-    let num_rows = state.len();
-    let num_cols = state[0].len();
-
-    let mut result: Vec<Vec<u8>> = vec![vec![0; num_cols]; num_rows];
-
-    for c in 0..num_cols {
-        let key_word = expanded_key[(round_key * n_b) + c].to_be_bytes();
-
-        result[0][c] = state[0][c] ^ key_word[0];
-        result[1][c] = state[1][c] ^ key_word[1];
-        result[2][c] = state[2][c] ^ key_word[2];
-        result[3][c] = state[3][c] ^ key_word[3];
-    }
-
-    result
-        .into_iter()
-        .map(|row| row.into_iter().map(Into::into).collect())
-        .collect()
-}
-
-fn cipher(input: impl AsRef<[u8]>, expanded_key: &[u32], key_type: KeyType) -> Vec<u8> {
+fn inverse_cipher(input: impl AsRef<[u8]>, expanded_key: &[u32], key_type: KeyType) -> Vec<u8> {
     let n_b = key_type.n_b();
     let n_r = key_type.n_r();
     let state = input
@@ -129,19 +97,20 @@ fn cipher(input: impl AsRef<[u8]>, expanded_key: &[u32], key_type: KeyType) -> V
             result
         });
 
-    let state = add_round_key(state, expanded_key, 0, n_b);
+    let state = add_round_key(state, expanded_key, n_r, n_b);
 
-    let state = (1..n_r).into_iter().fold(state, |state, round| {
-        let state = sub_bytes(state);
+    let state = (1..n_r).into_iter().rev().fold(state, |state, round| {
+		println!("{}", round);
         let state = shift_rows(state);
-        let state = mix_columns(state);
+        let state = sub_bytes(state);
         let state = add_round_key(state, expanded_key, round, n_b);
+        let state = mix_columns(state);
         state
     });
 
-    let state = sub_bytes(state);
     let state = shift_rows(state);
-    let state = add_round_key(state, expanded_key, n_r, n_b);
+    let state = sub_bytes(state);
+    let state = add_round_key(state, expanded_key, 0, n_b);
 
     let mut result = Vec::with_capacity(state.len() * state[0].len());
     for i in 0..state[0].len() {
@@ -159,18 +128,18 @@ mod tests {
     #[test]
     fn sub_bytes_test() {
         let state = [
-            [0x19, 0xa0, 0x9a, 0xe9],
-            [0x3d, 0xf4, 0xc6, 0xf8],
-            [0xe3, 0xe2, 0x8d, 0x48],
-            [0xbe, 0x2b, 0x2a, 0x08],
-        ];
-
-        let state = sub_bytes(state);
-        let sub = [
             [0xd4, 0xe0, 0xb8, 0x1e],
             [0x27, 0xbf, 0xb4, 0x41],
             [0x11, 0x98, 0x5d, 0x52],
             [0xae, 0xf1, 0xe5, 0x30],
+        ];
+
+        let state = sub_bytes(state);
+        let sub = [
+            [0x19, 0xa0, 0x9a, 0xe9],
+            [0x3d, 0xf4, 0xc6, 0xf8],
+            [0xe3, 0xe2, 0x8d, 0x48],
+            [0xbe, 0x2b, 0x2a, 0x08],
         ];
         assert_eq!(state, sub);
     }
@@ -179,17 +148,17 @@ mod tests {
     fn shift_rows_test() {
         let state = [
             [0xd4, 0xe0, 0xb8, 0x1e],
-            [0x27, 0xbf, 0xb4, 0x41],
-            [0x11, 0x98, 0x5d, 0x52],
-            [0xae, 0xf1, 0xe5, 0x30],
+            [0xbf, 0xb4, 0x41, 0x27],
+            [0x5d, 0x52, 0x11, 0x98],
+            [0x30, 0xae, 0xf1, 0xe5],
         ];
 
         let state = shift_rows(state);
         let shift = [
             [0xd4, 0xe0, 0xb8, 0x1e],
-            [0xbf, 0xb4, 0x41, 0x27],
-            [0x5d, 0x52, 0x11, 0x98],
-            [0x30, 0xae, 0xf1, 0xe5],
+            [0x27, 0xbf, 0xb4, 0x41],
+            [0x11, 0x98, 0x5d, 0x52],
+            [0xae, 0xf1, 0xe5, 0x30],
         ];
         assert_eq!(state, shift);
     }
@@ -197,56 +166,27 @@ mod tests {
     #[test]
     fn mix_columns_state() {
         let state = [
+            [0x04, 0xe0, 0x48, 0x28],
+            [0x66, 0xcb, 0xf8, 0x06],
+            [0x81, 0x19, 0xd3, 0x26],
+            [0xe5, 0x9a, 0x7a, 0x4c],
+        ];
+
+        let state = mix_columns(state);
+        let mix = [
             [0xd4, 0xe0, 0xb8, 0x1e],
             [0xbf, 0xb4, 0x41, 0x27],
             [0x5d, 0x52, 0x11, 0x98],
             [0x30, 0xae, 0xf1, 0xe5],
         ];
-
-        let state = mix_columns(state);
-        let mix = [
-            [0x04, 0xe0, 0x48, 0x28],
-            [0x66, 0xcb, 0xf8, 0x06],
-            [0x81, 0x19, 0xd3, 0x26],
-            [0xe5, 0x9a, 0x7a, 0x4c],
-        ];
         assert_eq!(state, mix);
-    }
-
-    #[test]
-    fn add_round_key_test() {
-        let state = [
-            [0x04, 0xe0, 0x48, 0x28],
-            [0x66, 0xcb, 0xf8, 0x06],
-            [0x81, 0x19, 0xd3, 0x26],
-            [0xe5, 0x9a, 0x7a, 0x4c],
-        ];
-
-        let expanded_key: [u32; 44] = [
-            0x2b7e1516, 0x28aed2a6, 0xabf71588, 0x09cf4f3c, 0xa0fafe17, 0x88542cb1, 0x23a33939,
-            0x2a6c7605, 0xf2c295f2, 0x7a96b943, 0x5935807a, 0x7359f67f, 0x3d80477d, 0x4716fe3e,
-            0x1e237e44, 0x6d7a883b, 0xef44a541, 0xa8525b7f, 0xb671253b, 0xdb0bad00, 0xd4d1c6f8,
-            0x7c839d87, 0xcaf2b8bc, 0x11f915bc, 0x6d88a37a, 0x110b3efd, 0xdbf98641, 0xca0093fd,
-            0x4e54f70e, 0x5f5fc9f3, 0x84a64fb2, 0x4ea6dc4f, 0xead27321, 0xb58dbad2, 0x312bf560,
-            0x7f8d292f, 0xac7766f3, 0x19fadc21, 0x28d12941, 0x575c006e, 0xd014f9a8, 0xc9ee2589,
-            0xe13f0cc8, 0xb6630ca6,
-        ];
-
-        let state = add_round_key(state, &expanded_key, 1, 4);
-        let round = [
-            [0xa4, 0x68, 0x6b, 0x02],
-            [0x9c, 0x9f, 0x5b, 0x6a],
-            [0x7f, 0x35, 0xea, 0x50],
-            [0xf2, 0x2b, 0x43, 0x49],
-        ];
-        assert_eq!(state, round);
     }
 
     #[test]
     fn cipher_test() {
         let input = [
-            0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37,
-            0x07, 0x34,
+            0x39, 0x25, 0x84, 0x1d, 0x02, 0xdc, 0x09, 0xfb, 0xdc, 0x11, 0x85, 0x97, 0x19, 0x6a,
+            0x0b, 0x32,
         ];
 
         let expanded_key: [u32; 44] = [
@@ -260,11 +200,11 @@ mod tests {
         ];
 
         let expected: [u8; 16] = [
-            0x39, 0x25, 0x84, 0x1d, 0x02, 0xdc, 0x09, 0xfb, 0xdc, 0x11, 0x85, 0x97, 0x19, 0x6a,
-            0x0b, 0x32,
+            0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37,
+            0x07, 0x34,
         ];
 
-        let result: Vec<u8> = cipher(input, &expanded_key, KeyType::Aes128);
+        let result: Vec<u8> = inverse_cipher(input, &expanded_key, KeyType::Aes128);
 
         for val in expected {
             print!("0x{:02x} ", val);
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod finite_field;
 mod constant;
 mod key_expansion;
 mod cipher;
+mod inverse_cipher;
 
 fn main() {
     println!("Hello, world!");
